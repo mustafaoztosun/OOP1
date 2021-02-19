aa2244f689cd2367233c9493112d582b4be3e383
16 Ocak Ders Sonu <mustafaoztosun.g@gmail.com>
2021-02-19 22:34:01 +0300

    23 Ocak - OOP3

---
+++ OoP2/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OoP2
+{
+    //İş Sınıfları // business classes
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+        }
+    }
+}
---
+++ OoP2/TuzelMusteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OoP2
+{
+    //miras inheritance 
+    class TuzelMusteri:Musteri
+    {
+ 
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+
+    }
+}
